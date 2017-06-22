d770f87cb49b307cb6b1baf55a1a606fb054d760
Leland Krych <Leland.Krych@gmail.com>
2017-06-22 16:18:57 -0700

    Initialize

---
+++ note-app/app.js
@@ -0,0 +1,23 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes');
+
+const argv = yargs.argv;
+let command = argv._[0];
+
+if (command === "add"){
+  notes.addNote(argv.title, argv.body);
+} else if (command === "list"){
+  notes.getAll();
+} else if (command === "read"){
+  notes.readNote(argv.title);
+} else if (command === "remove"){
+  notes.removeNote(argv.title);
+}
+ else{
+  console.log('Command not recognized.');
+}
