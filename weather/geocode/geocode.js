0877658a5f400bc87662d3e4ac5695fc8bf6d91f
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 10:02:25 -0700

    refactor geocode to utilize callback

--- weather/geocode/geocode.js
+++ weather/geocode/geocode.js
@@ -1,24 +1,26 @@
 const request = require('request');
 
-const geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
   request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-  }, (error, response, body) => {
-  if(error){
-    //if your network is down
-    console.log("Unable to connect to Google servers.");
-  }
-  else if (body.status === "ZERO_RESULTS") {
-    console.log("Unable to find that address.");
-  }
-  else if (body.status === "OK"){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
-  }
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+    }, (error, response, body) => {
+    if(error){
+      //if your network is down
+      callback("Unable to connect to Google servers.");
+    }
+    else if (body.status === "ZERO_RESULTS") {
+      callback("Unable to find that address.");
+    }
+    else if (body.status === "OK"){
+      callback(undefined,{
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    }
   });
 };
 
