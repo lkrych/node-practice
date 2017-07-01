c41814a729d6e26a4e0ffe43df5a6a4cd1082cab
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 09:43:01 -0700

    Add error handling to request

--- weather/app.js
+++ weather/app.js
@@ -20,7 +20,16 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    //if your network is down
+    console.log("Unable to connect to Google servers.");
+  }
+  else if (body.status === "OK"){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+  }
+  else {
+    console.log("Unable to find that address.");
+  }
 });
