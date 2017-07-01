bcbad6be1ab0c2f90a5113f0ec49353f42c9afb8
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 09:33:02 -0700

    Create dynamic API request through command line

--- weather/app.js
+++ weather/app.js
@@ -1,7 +1,21 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true //always parse the address as a string
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
