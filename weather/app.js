95f5e5583a1a7e3ea0612ffc129a7b908104e7b1
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 19:49:49 -0700

    Add basic request for weather fetching

--- weather/app.js
+++ weather/app.js
@@ -1,24 +1,35 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true //always parse the address as a string
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage){
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true //always parse the address as a string
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+const keys = require('./api_keys.js');
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage){
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
+request({
+url: `https://api.forecast.io/forecast/${keys.forecastIOKey}/37.8013728,-122.4243798`,
+json: true
+}, (error, response, body) => {
+  console.log(body.currently.temperature);
 });
