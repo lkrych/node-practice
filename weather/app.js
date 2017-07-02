1f6302c8f9c4e72620d6f5c90baad7c3644e7e6f
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 21:10:06 -0700

    create getForecast callback

--- weather/app.js
+++ weather/app.js
@@ -1,35 +1,33 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true //always parse the address as a string
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage){
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-const request = require('request');
+const geocode = require('./geocode/geocode');
+const forecast = require('./forecast/forecast');
 
-const keys = require('./api_keys.js');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true //always parse the address as a string
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-request({
-url: `https://api.forecast.io/forecast/${keys.forecastIOKey}/37.8013728,-122.4243798`,
-json: true
-}, (error, response, body) => {
-  console.log(body.currently.temperature);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage){
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
+});
+
+forecast.getForecast(37.801372, -122.4243798, (errorMessage, weatherResults) => {
+  if (errorMessage){
+    console.log(errorMessage);
+  }else {
+    console.log(JSON.stringify(weatherResults, undefined, 2));
+  }
 });
---
+++ weather/forecast/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const keys = require('../api_keys.js');
+
+var getForecast = (lat, lng, callback) => {
+  request({
+  url: `https://api.forecast.io/forecast/${keys.forecastIOKey}/${lat},${lng}`,
+  json: true
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200){
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
+    } else{
+      callback('Unable to fetch weather.');
+    }
+  });
+};
+
+module.exports.getForecast = getForecast;
