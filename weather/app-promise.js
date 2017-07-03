78c752dcd9a83ff6157eca30c35bd6ab8b2c10f9
Leland Krych <Leland.Krych@gmail.com>
2017-07-02 22:33:02 -0700

    Add second promise for forecast

--- weather/app-promise.js
+++ weather/app-promise.js
@@ -1,5 +1,5 @@
 const yargs = require('yargs');
-
+const keys = require('./api_keys.js');
 const axios = require('axios');
 
 const argv = yargs
@@ -19,9 +19,22 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeURL).then((response) => {
-  console.log(response.data);
+  if(response.data.status === 'ZERO_RESULTS'){
+    throw new Error('Unable to find that address.');
+  }
+
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherURL = `https://api.forecast.io/forecast/${keys.forecastIOKey}/${lat},${lng}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherURL);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
 }).catch((e) => {
   if(e.code === "ENOTFOUND"){
     console.log("Unable to connect to API servers");
+  } else {
+    console.log(e.message);
   }
 });
