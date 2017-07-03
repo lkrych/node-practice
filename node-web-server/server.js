19c11655de5e6b64b6ea7a73c6612b5e257663d5
Leland Krych <Leland.Krych@gmail.com>
2017-07-02 22:54:03 -0700

    Add multiple routes to express server

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/', (request, response) =>{
-  response.send('Hello, Express!');
+  response.send({
+    name: 'Fideo',
+    likes: [
+      'noodles',
+      'chicken'
+    ]
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.send('About page.');
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    error: 'Unable to handle this request'
+  });
 });
 
 app.listen(3000);
