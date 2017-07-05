cbf05f4fa761fcd0021a3f04e3d5efd4223496c6
Leland Krych <Leland.Krych@gmail.com>
2017-07-05 12:09:00 -0700

    Add testcase for square

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,5 +3,16 @@ const utils = require('./utils');
 
 it('Should add two numbers.', () => {
   var res = utils.add(33, 11);
-  
+
+  if(res !== 44){
+    throw new Error(`Expected 44, but got ${res}`);
+  }
+});
+
+it('Should square a number.', () => {
+  var res = utils.square(9);
+
+  if(res !== 81){
+    throw new Error(`Expected 81, but got ${res}`);
+  }
 });
