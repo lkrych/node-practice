8a9a6ff06860b0a4ecbc620a9f0699809b75bb77
Leland Krych <Leland.Krych@gmail.com>
2017-07-06 07:45:14 -0700

    create spy test

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -1,11 +1,28 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
+
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db); //replace db with db spy
 
   it('should call the spy correctly', () => {
     var spy =  expect.createSpy();
     spy('Andrew', 25);
     expect(spy).toHaveBeenCalledWith('Andrew', 25);
+  });
+
+  it('should call saveUser with the user object', () => {
+    var email = 'fideo@noodle.com';
+    var password = '123abc';
+
+    app.handleSignup(email, password);
+
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
   });
 
 });
