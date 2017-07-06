a871341332344fc1465077b7648318b7f9fd38de
Leland Krych <Leland.Krych@gmail.com>
2017-07-05 22:07:29 -0700

    Add describe blocks for server

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -4,28 +4,34 @@ const expect = require('expect');
 var app = require('./server').app;
 
 describe('Server', () => {
-  it('should return Hello world! response', (done) => {
-    request(app)
-      .get('/')
-      .expect(404)
-      .expect((res) => {
-        expect(res.body).toInclude({ // use expect library matchers!
-          error: 'Page not found.'
-        });
-      })
-      .end(done);
+
+  describe("#GET /", () => {
+    it('should return Hello world! response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({ // use expect library matchers!
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
   });
 
-  it('should return array of user objects', (done) => {
-    request(app)
-      .get('/users')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toInclude({ // use expect library matchers!
-          name: 'Fideo',
-          age: 43
-        });
-      })
-      .end(done);
+  describe("GET /users", () =>{
+    it('should return array of user objects', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({ // use expect library matchers!
+            name: 'Fideo',
+            age: 43
+          });
+        })
+        .end(done);
+    });
   });
+
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,31 +2,36 @@ const expect = require('expect');
 const utils = require('./utils');
 
 describe('Utils', () => {
-  
-  it('Should add two numbers.', () => {
-    var res = utils.add(33, 11);
-    expect(res).toBe(44).toBeA('number');
-  });
 
-  //done argument tells mocha that the test is aync
-  it('Should add two numbers asynchronously.', (done) => {
-    var res = utils.asyncAdd(4,3, (sum) => {
-      expect(sum).toBe(7).toBeA('number');
-      done(); // exits mocha
+  describe("#add", () => {
+    it('Should add two numbers.', () => {
+      var res = utils.add(33, 11);
+      expect(res).toBe(44).toBeA('number');
+    });
+
+    //done argument tells mocha that the test is aync
+    it('Should add two numbers asynchronously.', (done) => {
+      var res = utils.asyncAdd(4,3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done(); // exits mocha
+      });
     });
   });
 
-  it('Should square a number.', () => {
-    var res = utils.square(9);
-    expect(res).toBe(81).toBeA('number');
-  });
+  describe("#square", () => {
+    it('Should square a number.', () => {
+      var res = utils.square(9);
+      expect(res).toBe(81).toBeA('number');
+    });
 
-  it('Should square a number asynchronously.', (done) => {
-    var res = utils.asyncSquare(4, (square) => {
-      expect(square).toBe(16).toBeA('number');
-      done(); // exits mocha
+    it('Should square a number asynchronously.', (done) => {
+      var res = utils.asyncSquare(4, (square) => {
+        expect(square).toBe(16).toBeA('number');
+        done(); // exits mocha
+      });
     });
   });
+
 
   it('should expect some values', () => {
     // expect({name: 'Fideo'}).toBe({name: 'Fideo'}); This will fail
