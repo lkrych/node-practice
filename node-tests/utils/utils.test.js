272bdc98af14421f9d9fe519aa86c15878ec72db
Leland Krych <Leland.Krych@gmail.com>
2017-07-05 20:27:08 -0700

    refactor tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,12 +3,10 @@ const utils = require('./utils');
 
 it('Should add two numbers.', () => {
   var res = utils.add(33, 11);
-  expect(res).toBe(44);
-  expect(res).toBeA('number');
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('Should square a number.', () => {
   var res = utils.square(9);
-  expect(res).toBe(81);
-  expect(res).toBeA('number');
+  expect(res).toBe(81).toBeA('number');
 });
