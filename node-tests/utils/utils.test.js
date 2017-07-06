62594fa85c80372bafc4cf30ae9a7d495e7e8599
Leland Krych <Leland.Krych@gmail.com>
2017-07-05 20:30:18 -0700

    demonstrate difference between toBe and toEqual

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -10,3 +10,9 @@ it('Should square a number.', () => {
   var res = utils.square(9);
   expect(res).toBe(81).toBeA('number');
 });
+
+it('should expect some values', () => {
+  // expect({name: 'Fideo'}).toBe({name: 'Fideo'}); This will fail
+  // They are not the exact same object. They have the same properties
+  expect({name: 'Fideo'}).toEqual({name: 'Fideo'});
+});
