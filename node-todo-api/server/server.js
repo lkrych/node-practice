ce8e26a4b38ffcbd0718741de41b08cad4b0dd16
Leland Krych <Leland.Krych@gmail.com>
2017-07-16 14:19:55 -0700

    Add post endpoint

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -7,7 +7,19 @@ var { User } = require('./models/user');
 
 var app = express();
 
+app.use(bodyParser.json()); //give bodyParser to express middleware
 
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) =>{
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
