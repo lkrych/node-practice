f598529d617f2731c1e87709ba49aedd232acfa0
Leland Krych <Leland.Krych@gmail.com>
2017-07-16 14:11:10 -0700

    Setup express server skeleton

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,1 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
