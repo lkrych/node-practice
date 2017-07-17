522422f611c758dcd643117476dd3fdd575f45dc
Leland Krych <Leland.Krych@gmail.com>
2017-07-16 19:16:31 -0700

    add Test case for POST

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -3,3 +3,33 @@ const request = require('supertest');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done()); //wipes all todos
+});
+
+describe('POST /todos', () => {
+  //you need done to do an async test
+  it('should create a new todo', (done) => {
+    var text = 'Test todo test';
+
+    request(app) //supertest library
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text); //expect response to have text in body
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err); //if err exists, wraps up test
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+});
