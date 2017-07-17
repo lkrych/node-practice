1ab92cb193b49d714dda53052b1db8d385f8d0a0
Leland Krych <Leland.Krych@gmail.com>
2017-07-16 19:23:48 -0700

    Add test case for validation failure

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -14,22 +14,38 @@ describe('POST /todos', () => {
     var text = 'Test todo test';
 
     request(app) //supertest library
-    .post('/todos')
-    .send({text})
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.text).toBe(text); //expect response to have text in body
-    })
-    .end((err, res) => {
-      if(err){
-        return done(err); //if err exists, wraps up test
-      }
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text); //expect response to have text in body
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err); //if err exists, wraps up test
+        }
 
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
-        done();
-      }).catch((e) => done(e));
-    });
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
